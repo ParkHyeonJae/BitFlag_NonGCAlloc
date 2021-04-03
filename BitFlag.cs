0b723dfe207827b300736dc34bbcc16558033049
박현재 <37414117+ParkHyeonJae@users.noreply.github.com>
2021-04-03 23:45:04 +0900

    Add files via upload

---
+++ BitFlag.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Utilitys.BitFlag
+{
+    /// <summary>
+    /// Reference By https://www.slideshare.net/devcatpublications/enum-boxing-enum-ndc2019
+    /// </summary>
+    public class ValueCastTo
+    {
+        protected static class Cache<TFrom, TTo>
+        {
+            public static readonly Func<TFrom, TTo> Caster = Get();
+
+            static Func<TFrom, TTo> Get()
+            {
+                var p = Expression.Parameter(typeof(TFrom), "from");
+                var c = Expression.ConvertChecked(p, typeof(TTo));
+                return Expression.Lambda<Func<TFrom, TTo>>(c, p).Compile();
+            }
+        }
+    }
+
+    public class ValueCastTo<TTo> : ValueCastTo
+    {
+        public static TTo From<TFrom>(TFrom from)
+        {
+            return Cache<TFrom, TTo>.Caster(from);
+        }
+    }
+
+    public class BitFlag<T> where T : Enum
+    {
+        public int eStateFlag { get; private set; }
+
+        /// <summary>
+        /// Flag Turn On
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetOn(T state) => eStateFlag |= ValueCastTo<int>.From<T>(state);
+
+        /// <summary>
+        /// Flag Turn Off
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetOff(T state) => eStateFlag &= ~ValueCastTo<int>.From<T>(state);
+
+        /// <summary>
+        /// StateFlag Turn On Off
+        /// </summary>
+        /// <param name="state"></param>
+        public void Set(T state) => eStateFlag ^= ValueCastTo<int>.From<T>(state);
+
+        /// <summary>
+        /// Reset All State
+        /// </summary>
+        public void Clear() => eStateFlag = 0;
+
+        /// <summary>
+        /// Turn on All State
+        /// </summary>
+        public void SetAll() => eStateFlag = int.MaxValue;
+
+        /// <summary>
+        /// Turn On Exclude parameter States
+        /// </summary>
+        /// <param name="states"></param>
+        public void ExclusiveSetAll(params T[] states)
+        {
+            eStateFlag = int.MaxValue;
+            foreach (T state in states)
+            {
+                eStateFlag ^= ValueCastTo<int>.From<T>(state);
+            }
+        }
+
+        /// <summary>
+        /// Is Contains Flag
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public bool HasFlag(T state) => ((eStateFlag & ValueCastTo<int>.From<T>(state)) != 0);
+    }
+}
---
+++ BitFlagTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilitys.BitFlag;
+
+public class BitFlagTest : MonoBehaviour
+{
+    
+    enum Flags
+    {
+        ONE = 0x1,
+        Two = 0x01,
+        Three = 0x001,
+        Four = 0x0001,
+        Five = 0x00001
+    }
+
+    private BitFlag<Flags> bitFlag = new BitFlag<Flags>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bitFlag.Set(Flags.Four);
+
+        Debug.Log("HasFlag : " +  bitFlag.HasFlag(Flags.Four));
+    }
+
+}
